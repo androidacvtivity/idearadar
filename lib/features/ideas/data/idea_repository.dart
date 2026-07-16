c24e98d0f6b28c089b0cc790676c13ec394b87db
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2026-07-16 07:32:06 +0300

    feat: add research note repository operations

--- lib/features/ideas/data/idea_repository.dart
+++ lib/features/ideas/data/idea_repository.dart
@@ -1,4 +1,5 @@
 import 'package:idearadar/features/ideas/domain/idea.dart';
+import 'package:idearadar/features/ideas/domain/idea_note.dart';
 
 abstract interface class IdeaRepository {
   Future<void> initialize();
@@ -10,4 +11,12 @@ abstract interface class IdeaRepository {
   Future<void> updateIdea(Idea idea);
 
   Future<void> deleteIdea(String ideaId);
+
+  Future<List<IdeaNote>> getNotes(String ideaId);
+
+  Future<void> addNote(IdeaNote note);
+
+  Future<void> updateNote(IdeaNote note);
+
+  Future<void> deleteNote(String noteId);
 }
